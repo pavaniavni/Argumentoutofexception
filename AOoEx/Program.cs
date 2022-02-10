056491f9925662f2e7204e52820405b189e11b3b
pavani <pavaniavni456@gmail.com>
2022-02-10 11:02:58 +0530

    added arguoutofexcep

---
+++ AOoEx/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Argumentoutofexception
+{
+    public static void Main()
+    {
+        var list = new List<string>();
+
+        try
+        {
+            Console.WriteLine("The first item: '{0}'", list[0]);
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
